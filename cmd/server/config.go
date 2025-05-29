a80d465bc0cc394f34bfbaaff34f9a328601a4a3
Andrey Rafalskiy <a.rafalskiy@bi.zone>
2025-05-29 10:29:37 +0700

    Fix review notes

--- cmd/server/config.go
+++ cmd/server/config.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"flag"
+	"path/filepath"
+	"runtime"
 
 	"github.com/caarlos0/env/v10"
 
@@ -9,6 +11,11 @@ import (
 )
 
 func updateCfg() (config.Server, error) {
+	var migrationPath string
+	_, file, _, ok := runtime.Caller(0)
+	if ok {
+		migrationPath = filepath.Dir(file) + "/db/migrations"
+	}
 	cfg := config.Server{}
 	flag.BoolVar(&cfg.Restore, "r", false, "need to restore from the dump")
 	flag.StringVar(&cfg.Addr, "a", config.DefaultAddr, "address and port to run server")
@@ -17,7 +24,7 @@ func updateCfg() (config.Server, error) {
 	flag.StringVar(&cfg.DumpPath, "f", config.DefaultDumpPath, "dump file path")
 	flag.IntVar(&cfg.StorePeriod, "i", config.DefaultDumpInterval, "dump interval in seconds")
 	flag.StringVar(&cfg.DB.DSN, "d", "", "database address")
-	flag.StringVar(&cfg.DB.MigrationsPath, "m", "", "path to migrations")
+	flag.StringVar(&cfg.DB.MigrationsPath, "m", migrationPath, "path to migrations")
 	flag.StringVar(&cfg.DB.MigrationsTable, "t", "", "name of migration table, where migrator writes own data")
 	flag.StringVar(&cfg.Key, "k", "", "key for sign")
 	flag.Parse()
